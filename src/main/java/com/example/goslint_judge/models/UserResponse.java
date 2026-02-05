403b27b92356cb77feabcc3d06d7e44745d88bea
JOR3NTO <jorgeedcobo@gmail.com>
2026-02-05 17:55:37 -0500

    Campos extras de usuarios estudiantes

--- src/main/java/com/example/goslint_judge/models/UserResponse.java
+++ src/main/java/com/example/goslint_judge/models/UserResponse.java
@@ -2,17 +2,38 @@ package com.example.goslint_judge.models;
 
 import java.time.Instant;
 
+
 public class UserResponse {
 
     private Long id;
     private String username;
     private String email;
     private Instant createdAt;
+    private String firstName;
+    private String lastName;
+    private String university;
+    private String country;
+    private String studentId;
 
-    public UserResponse(Long id, String username, String email, Instant createdAt) {
+
+    public UserResponse(Long id,
+                        String username,
+                        String email,
+                        Instant createdAt,
+                        String firstName,
+                        String lastName,
+                        String university,
+                        String country,
+                        String studentId, Instant createdAt1) {
         this.id = id;
         this.username = username;
         this.email = email;
+        this.createdAt = createdAt;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.university = university;
+        this.country = country;
+        this.studentId = studentId;
         this.createdAt = createdAt;
     }
 
@@ -48,6 +69,44 @@ public class UserResponse {
         this.createdAt = createdAt;
     }
 
-    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
 
 }
